fdfe56d65eb1a958fb2109c625517fc00746cf6a
JingYunKaiFa <431190086@qq.com>
2021-04-08 18:10:24 +0800

    Project catalog basics

---
+++ Rear stand/WallpaperApplet/src/main/java/com/example/whalecrane/WhalecraneApplication.java
@@ -0,0 +1,13 @@
+package com.example.whalecrane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhalecraneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhalecraneApplication.class, args);
+    }
+
+}
---
+++ Rear stand/WallpaperApplet/src/test/java/com/example/whalecrane/WhalecraneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whalecrane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhalecraneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
